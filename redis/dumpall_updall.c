04ba2546400f6824f85a7c504cf9a5e5dcbdd21d
Karla Saur <ksaur@umd.edu>
2014-11-10 13:49:25 -0500

    working on iterating through keys in database for updating. todo: call my new update function

--- redis/dumpall_updall.c
+++ redis/dumpall_updall.c
@@ -1,6 +1,10 @@
 /* dumpall_updateall.c:  This is a first step towards updating JSON strings in
  * redis.  It gets all keys, and then updates them all at once.  All of this is
- * done outside of redis*/
+ * done outside of redis
+ *
+ * Assume you have some strings stored as keys mapping to JSON objects.
+ * This will iterate over all keys and run the generated update functions.
+ **/
 
 #include <hiredis/hiredis.h>
 #include <jansson.h>
@@ -26,20 +30,62 @@ void redisconnect(const char * host, int port)
       }
       exit(1);
    }
-   /* wipe out all old keys from previous run */
-   reply =redisCommand(redis, "FLUSHDB");
-   if(strncmp(reply->str,"OK", 2) != 0)
-      printf("WARNING. Could not flush old keys.\n");
-   freeReplyObject(reply);
 }
 
 int main(int argc, char *argv[])
 {
 
+   int i;
    const char *host = (argc > 1) ? argv[1] : "127.0.0.1";
    int port = (argc > 2) ? atoi(argv[2]) : 6379;
+   json_t *old, *new;
+   json_error_t error;
+   const char *key;
+   json_t *newvalue, *oldvalue;
 
    redisconnect(host, port);
+
+   /* iterate through all keys */
+   redisReply * all_keys_arr = redisCommand(redis, "KEYS %s", "*");
+   for(i=0; i<all_keys_arr->elements; i++)
+   {
+      char * curr_key = all_keys_arr->element[i]->str;
+      printf("Found key = %s\n", curr_key);
+      /* check to make sure that the type of key is a string */
+
+      reply = redisCommand(redis, "TYPE %s", curr_key);
+      if(strncmp(reply->str, "string", 6) !=0 )
+      {
+         printf ("TODO: key (%s) not a string.  Not implemented!!!\n", curr_key);
+         freeReplyObject(reply);
+      }
+      else
+      {
+         freeReplyObject(reply);
+         reply = redisCommand(redis, "GET %s", curr_key);
+         printf ("Value for (%s) is %s \n", curr_key, reply->str);
+         old = json_loads(reply->str, 0, &error);
+         if (json_is_object(old))
+         {
+            printf("successfully loaded object into json for key %s\n", curr_key);
+			/* Grab the head key.  Assuming for now that there is just one json
+             * object per redis entry*/
+            json_object_foreach(old, key, oldvalue){
+                printf("key = %s\n", key);
+
+            }
+         }
+         else
+         {
+
+            printf("Failed to load object into json for key %s\n", curr_key);
+         }
+
+         freeReplyObject(reply);
+      }
+
+   }
+   freeReplyObject(all_keys_arr);
 
    return 0;
 }
