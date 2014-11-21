bd6fbb08ca30257d77283108306c4dc851c5829c
Karla Saur <ksaur@umd.edu>
2014-11-21 09:08:59 -0500

    script for bulk data

---
+++ example_jsonbulk/sample_generate.py
@@ -0,0 +1,14 @@
+s = "{   \"menu \": {   \"id \": 1,   \"live \": true,   \"deleteme \": true,   \"pointer \": null,   \"value \":  \"File \",   \"popup \": {   \"menuitem \": [  {   \"value \":  \"New \",   \"onclick \":  \"CreateNewDoc() \"  },  {   \"value \":  \"Open \",   \"onclick \":  \"OpenDoc() \"  },  {   \"value \":  \"Close \",   \"onclick \":  \"CloseDoc() \"  }  ]  }  },   \"soup \": {   \"delme \": 9,   \"dontmodmes \": 4  } }"
+f= open("data.txt", 'w')
+i=1
+for x in range(1, 100):
+     s = s.replace("id \": "+str(i), "id \": "+str(i+1))
+     if (x % 3 == 0):
+         t = s.replace("CreateNewDoc", "EditExisting")
+         t = t.replace("New", "Edit")
+         t = t.replace("true", "false")
+         f.write(t + "\n")
+     else:
+         f.write(s + "\n")
+     i+=1
+
