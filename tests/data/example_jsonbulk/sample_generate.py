8c4a956a3038d68e74e062a496267572086552c9
Karla Saur <ksaur@umd.edu>
2015-02-28 13:07:23 -0500

    preparing bulk data loading

--- tests/data/example_jsonbulk/sample_generate.py
+++ tests/data/example_jsonbulk/sample_generate.py
@@ -1,14 +1,67 @@
-s = "{   \"menu\": {   \"id\": 1,   \"live\": true,   \"deleteme\": true,   \"pointer\": null,   \"value\":  \"File\",   \"popup\": {   \"menuitem\": [  {   \"value\":  \"New\",   \"onclick\":  \"CreateNewDoc()\"  },  {   \"value\":  \"Open\",   \"onclick\":  \"OpenDoc()\"  },  {   \"value\":  \"Close\",   \"onclick\":  \"CloseDoc()\"  }  ]  }  },   \"soup\": {   \"delme\": 9,   \"dontmodmes\": 4  } }"
-f= open("data.txt", 'w')
-i=1
-for x in range(1, 100):
-     s = s.replace("id\": "+str(i), "id\": "+str(i+1))
-     if (x % 3 == 0):
-         t = s.replace("CreateNewDoc", "EditExisting")
-         t = t.replace("New", "Edit")
-         t = t.replace("true", "false")
-         f.write(t + "\n")
-     else:
-         f.write(s + "\n")
-     i+=1
+import sys
+import redis, json
+import argparse
+import random, string
+import datetime
+from datetime import timedelta
+from random import randint
 
+
+def random_date(start, end):
+    return start + timedelta(
+        seconds=randint(0, int((end - start).total_seconds())))
+
+
+def gen_1_sadalage(r, num_entries):
+
+    for i in range(num_entries):
+        s = dict()
+        s["_id"]= random.randint(100000000,sys.maxint)
+        s["customerid"]= i
+        s["name"] = ''.join(random.choice(string.ascii_uppercase) for j in range(16)) 
+        since = random_date(datetime.date(2000,3,12), datetime.date(2008,1,1))
+        s["since"] = str(since)
+        o = dict()
+        o["orderid"] = random.randint(1000,10000000) 
+        o["orderdate"] = str(random_date(since, datetime.date(2015,1,1)))
+        l = list()
+        for it in range(random.randint(1,4)):
+            price = "{:.2f}".format(random.randint(2000,5000)/100.00)
+            prod = ''.join(random.choice(string.ascii_uppercase) for j in range(9))
+            items = {"product" : prod, "price": price}
+            l.append(items)
+        o["orderItems"] = l
+        s["order"] = o
+        r.set("customer:"+str(i), json.dumps(s))
+
+    print r.keys("customer:*")
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--e', nargs=1, help='number of entries ')
+    args = parser.parse_args()
+
+    if (args.e) is not None:
+        num_entries = int(args.e[0])
+    else:
+        print "using default number of entries (1000)" 
+        num_entries = 1000
+
+    r = redis.StrictRedis()
+    try:
+        r.ping()
+    except r.ConnectionError as e:
+        print(e)
+        sys.exit(-1)
+    r.flushall()
+
+    gen_1_sadalage(r, num_entries)
+
+
+    
+
+
+if __name__ == '__main__':
+    main()
