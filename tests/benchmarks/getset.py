7804e0757a861491593192d332c25ce4ba68cc22
Karla Saur <ksaur@umd.edu>
2015-03-01 13:21:36 -0500

    getset bench script

--- tests/benchmarks/getset.py
+++ tests/benchmarks/getset.py
@@ -9,30 +9,62 @@ against the lazy update redis client (lazyupdredis).
 import json
 import sys, os
 import redis
+import random, time
 from lazyupdredis import *
 from threading import Thread
+from time import sleep
+from random import randint
 
 
 
-def bench_normal_getset():
-    return None
 
+def do_getset(r, num_getsets, key_range):
 
-def bench_lazy_getset():
-    return None
+    for i in range(num_getsets):
+        rand1 = str(random.randint(0, key_range))
+        rand2 = str(random.randint(0, key_range))
+        r.set("edgeattr:" + rand1, "str"+rand1)
+        r.get("edgeattr:" + rand2)
+    
+
+def bench_getset(tname, num_clients, num_getsets, keyrange, args):
+    """ 
+    args (list) are for lazyredis.  If no args given, use normal StrictRedis
+    
+    """
+
+    print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  STARTING  " + tname + "  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+    start = time.time()
+    thread_arr = list()
+    for t in range(num_clients):
+        if args:
+            r = lazyupdredis.connect(args)
+        else:
+            r = redis.StrictRedis()
+        thread = (Thread(target = do_getset, args = (r, num_getsets, keyrange)))
+        thread_arr.append(thread)
+        thread.start()
+
+    for t in thread_arr:
+        print "joining" + str(t)
+        t.join()
+
+    end = time.time()
+    print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>  SUCCESS  ("+tname+")  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+    return end - start
 
 
 
 def main():
 
-    # Remove the previous run's generated files, for sanity's sake.
-    os.system("rm /tmp/gen*")
-
     # non-hooked redis commands to work as orginally specified
     actualredis = redis.StrictRedis()
+    actualredis.flushall()
 
-
-    actualredis.execute_command('QUIT')
+    print bench_getset("normal_redis_getset", 5, 5000, 5000, None)
+    actualredis.flushall()
+    print bench_getset("lazy_redis_getset", 5, 5000, 5000, [("edgeattr", "v0")])
 
 
 if __name__ == '__main__':
