3e0a0a4dac30d3ea6f43b28df8bb8076a7e35c1e
ksaur <ksaur@umd.edu>
2015-03-31 11:48:19 -0400

    exp3 scr

--- tests/exper3.sh
+++ tests/exper3.sh
@@ -2,7 +2,7 @@
 # since python seems to be doing weird when not restarted from scratch
 for i in {1..11}
 do
-     pypy experiment3.py do_get_or_set
+     #pypy experiment3.py do_get_or_set
      #pypy experiment3.py do_get
-     #pypy experiment3.py do_set
+     pypy experiment3.py do_set
 done
