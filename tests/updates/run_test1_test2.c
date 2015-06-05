2218c0684fda9eb68b78d768e2d42df7f559ded4
Karla Saur <ksaur@umd.edu>
2015-06-05 14:16:52 -0400

    fixed up tests to setup/teardown redis

--- tests/updates/run_test1_test2.c
+++ tests/updates/run_test1_test2.c
@@ -1,13 +1,21 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <hiredis/hiredis.h>
 
+#define DEBUG
 
-void check(redisReply *reply, char * expected){
+
+void check(int test_num, redisReply *reply, char * expected){
+#ifdef DEBUG
+  printf("(%d) Expected: %s, Got: %s\n", test_num, expected, reply->str);
+#endif
   assert(strncmp(reply->str, expected, strlen(expected))==0);
   freeReplyObject(reply);
 }
+
+
 
 
 /* Test updating with two namespaces.  Perform the updates on the two
@@ -17,29 +25,35 @@ void test_1_and_2_separate(void){
 
   redisReply *reply;
 
+  system("../../redis-2.8.17/src/redis-server ../../redis-2.8.17/redis.conf &");
+  sleep(2);
+  printf("Inside test_1_and_2_separate\n");
+
   redisContext * c = redisConnect("127.0.0.1", 6379);
   reply = redisCommand(c, "client setname %s", "order@v0,user@u0");
-  check(reply, "OK");
+  check(1, reply, "OK");
 
   reply = redisCommand(c,"SET %s %s", "order:111", "ffff");
-  check(reply, "OK");
+  check(2, reply, "OK");
   reply = redisCommand(c,"SET %s %s", "user:bbbb", "9999");
-  check(reply, "OK");
+  check(3, reply, "OK");
 
+  printf("about to load update\n");
   reply = redisCommand(c,"client setname %s", 
        "update/home/ksaur/AY1415/schema_update/tests/updates/test1.so");
-  check(reply, "OK");
+  check(4, reply, "OK");
 
+  printf("done loading update\n");
   reply = redisCommand(c,"GET %s", "order:111");
-  check(reply, "ffffUPDATED");
+  check(5, reply, "ffffUPDATED");
   reply = redisCommand(c,"GET %s", "user:bbbb");
-  check(reply, "9999");
+  check(6, reply, "9999");
 
   reply = redisCommand(c,"client setname %s", 
        "update/home/ksaur/AY1415/schema_update/tests/updates/test2.so");
-  check(reply, "OK");
+  check(7, reply, "OK");
   reply = redisCommand(c,"GET %s", "foo:order:111");
-  check(reply, "ffffUPDATED");
+  check(8, reply, "ffffUPDATED");
 
   reply = redisCommand(c,"GET %s", "order:111");
   assert(reply->type == REDIS_REPLY_NIL);
@@ -48,7 +62,10 @@ void test_1_and_2_separate(void){
   reply = redisCommand(c,"keys %s", "*");
   assert(reply->elements == 2);
   freeReplyObject(reply);
-
+   
+  system("killall redis-server");
+  sleep(2);
+ 
 }
 
 
@@ -56,23 +73,25 @@ void test_1_and_2_separate(void){
  * other.*/
 void test_1_and_2_together(void){
   redisReply *reply;
+  system("../../redis-2.8.17/src/redis-server ../../redis-2.8.17/redis.conf &");
+  sleep(2);
 
   redisContext * c = redisConnect("127.0.0.1", 6379);
   reply = redisCommand(c, "client setname %s", "order@v0,user@u0");
-  check(reply, "OK");
+  check(101, reply, "OK");
 
   reply = redisCommand(c,"SET %s %s", "order:111", "ffff");
-  check(reply, "OK");
+  check(102, reply, "OK");
 
   reply = redisCommand(c,"client setname %s", 
        "update/home/ksaur/AY1415/schema_update/tests/updates/test1.so");
-  check(reply, "OK");
+  check(103, reply, "OK");
   reply = redisCommand(c,"client setname %s", 
        "update/home/ksaur/AY1415/schema_update/tests/updates/test2.so");
-  check(reply, "OK");
+  check(104, reply, "OK");
 
   reply = redisCommand(c,"GET %s", "foo:order:111");
-  check(reply, "ffffUPDATED");
+  check(105, reply, "ffffUPDATED");
 
   reply = redisCommand(c,"GET %s", "order:111");
   assert(reply->type == REDIS_REPLY_NIL);
@@ -82,11 +101,13 @@ void test_1_and_2_together(void){
   assert(reply->elements == 1);
   freeReplyObject(reply);
 
+  system("killall redis-server");
+  sleep(2);
 }
 
 int main(void){
 
-  //test_1_and_2_separate();
+  test_1_and_2_separate();
   test_1_and_2_together();
   printf("All pass.\n");
   return 0;
