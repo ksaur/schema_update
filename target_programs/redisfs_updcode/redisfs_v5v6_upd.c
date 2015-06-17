e188916759b6675ca4ddf3f2bb9d55d673940878
Karla Saur <ksaur@umd.edu>
2015-06-17 14:58:01 -0400

    compression working yay!  update for v5 v6 can read files.  TODO, still need to find a way to update the size and mtime within mu

--- target_programs/redisfs_updcode/redisfs_v5v6_upd.c
+++ target_programs/redisfs_updcode/redisfs_v5v6_upd.c
@@ -1,20 +1,48 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <zlib.h>
 #include "kvolve_upd.h"
 
 
-void dir_ns_change(char ** key, void ** value){
+void upd_fun_dir_ns_change(char ** key, void ** value, size_t * val_len){
     /* The new-version code will already query with the correct string,
        if we've reached this part of the update in the new namespace.
        Just preserve the string and return it */
-    size_t s = strlen((char*)*value)+1;
+    size_t s = strlen((char*)*key)+1;
     char * save = malloc(s);
     strcpy(save, *key);
     *key = save;
 }
-void add_compression(char ** key, void ** value){
-   //TODO
+void upd_fun_add_compression(char ** key, void ** value, size_t * val_len){
+    char * split = strrchr(*key, ':');
+    if((split == NULL) || (strncmp(":DATA", split, 5) != 0))
+        return;
+
+
+    size_t size = strlen((char*)*value);
+    char *compressed = malloc((size * 2) + 1);
+    uLongf compressed_len = ((size * 2) + 1); //uLongf from zlib
+    int ret = compress2((void *)compressed, &compressed_len, *value, size,
+                  Z_BEST_SPEED);
+    if (ret != Z_OK)
+    {
+        fprintf(stderr, "compress2() failed - aborting write for %s\n", *key);
+        free(compressed);
+    }
+    *value = compressed;
+    *val_len = compressed_len;
+    
+
+//        reply = redisCommand(_g_redis, "SET %s:INODE:%d:SIZE %d",
+//                             _g_prefix, inode, size);
+//        freeReplyObject(reply);
+//
+//        reply = redisCommand(_g_redis, "SET %s:INODE:%d:MTIME %d",
+//                             _g_prefix, inode, time(NULL));
+//        freeReplyObject(reply);
+
+
 }
 
 struct kvolve_upd_info * get_update_func_list(void){
@@ -29,7 +57,7 @@ struct kvolve_upd_info * get_update_func_list(void){
     head->to_vers = "v6";
     head->num_funs = 1;
     head->funs = calloc(head->num_funs, sizeof(kvolve_update_kv_fun));
-    head->funs[0] = dir_ns_change;
+    head->funs[0] = upd_fun_dir_ns_change;
     head->next = info2;
 
     /* Add compression to DATA members of the INODE namespace */
@@ -39,7 +67,7 @@ struct kvolve_upd_info * get_update_func_list(void){
     info2->to_vers = "v6";
     info2->num_funs = 1;
     info2->funs = calloc(info2->num_funs, sizeof(kvolve_update_kv_fun));
-    info2->funs[0] = add_compression;
+    info2->funs[0] = upd_fun_add_compression;
     info2->next = NULL;
 
     return head;
