c63ba0374dc61fb45fd3ad3f0b1190f537edc904
Karla Saur <ksaur@umd.edu>
2015-06-18 17:06:41 -0400

    updated redisfs update to match new spec

--- target_programs/redisfs_updcode/redisfs_v5v6_upd.c
+++ target_programs/redisfs_updcode/redisfs_v5v6_upd.c
@@ -6,10 +6,6 @@
 #include "kvolve_upd.h"
 
 
-//void upd_fun_dir_ns_change(char ** key, void ** value, size_t * val_len){
-//  /* Done automatically */
-//}
-
 void upd_fun_add_compression(char ** key, void ** value, size_t * val_len){
 
     char callstr[256];
@@ -22,7 +18,7 @@ void upd_fun_add_compression(char ** key, void ** value, size_t * val_len){
     if((split == NULL) || (strncmp(":DATA", split, 5) != 0))
         return;
 
-    // The next several lines from redisfs.c v.6 (ln 848)
+    /* The next several lines from redisfs.c v.6 (ln 848)*/
     char *compressed = malloc((*val_len * 2) + 1);
     uLongf compressed_len = ((*val_len * 2) + 1); //uLongf from zlib
     int ret = compress2((void *)compressed, &compressed_len, *value, *val_len,
@@ -33,12 +29,13 @@ void upd_fun_add_compression(char ** key, void ** value, size_t * val_len){
         free(compressed);
     }
 
-    /* Now set the modification time.  (see redisfs.6/src/redisfs.c:878). (We
-     *   don't need to set size, since the size of the decompressed data didn't change.)
+    /* Now set the modification time.  We don't need to set size, since the
+     * size of the decompressed data didn't change. (See
+     * redisfs.6/src/redisfs.c:878 for orig.)
      * Grab the prefix by substracting from the split point.(ex: "skx:INODE:inode")*/
     snprintf(inode_info, (split-*key)+1, "%s\0", *key);
     sprintf(callstr, "set %s:MTIME %d", inode_info, (int)time(NULL));
-    kvolve_user_call(callstr);
+    kvolve_upd_redis_call(callstr);
 
     /* Set the new value and length (don't touch key),
      * to be stored in redis by kvolve */
@@ -48,30 +45,8 @@ void upd_fun_add_compression(char ** key, void ** value, size_t * val_len){
 
 
 /* This is the update structure for redisfs.5 to redisfs.6 */
-struct kvolve_upd_info * get_update_func_list(void){
-
-    struct kvolve_upd_info * head = malloc(sizeof(struct kvolve_upd_info));
-    struct kvolve_upd_info * info2 = malloc(sizeof(struct kvolve_upd_info));
-
-    /* Change the namespace from skx to skx:DIR */
-    head->from_ns = "skx";
-    head->to_ns = "skx:DIR";
-    head->from_vers = "v5";
-    head->to_vers = "v6";
-    head->num_funs = 0;
-    head->funs = NULL;
-    head->next = info2;
-
-    /* Add compression to DATA members of the INODE namespace */
-    info2->from_ns = "skx:INODE";
-    info2->to_ns = "skx:INODE";
-    info2->from_vers = "v5";
-    info2->to_vers = "v6";
-    info2->num_funs = 1;
-    info2->funs = calloc(info2->num_funs, sizeof(kvolve_update_kv_fun));
-    info2->funs[0] = upd_fun_add_compression;
-    info2->next = NULL;
-
-    return head;
+void kvolve_declare_update(){
+    kvolve_upd_spec("skx", "skx:DIR", "v5", "v6", 0);
+    kvolve_upd_spec("skx:INODE", "skx:INODE", "v5", "v6", 1, upd_fun_add_compression);
 }
 
