ec5a72011fd350a89d775e44917ab10e8ac3f247
Karla Saur <ksaur@umd.edu>
2015-06-21 14:59:57 -0400

    amico v1.2 v2.0 upd code

---
+++ target_programs/amico_updcode/run_v12v20.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/ksaur/AY1415/schema_update/tests/redis_server_tests/do_upd amico:followers@v1.2,amico:following@v1.2,amico:blocked@v1.2,amico:reciprocated@v1.2,amico:pending@v1.2 update/home/ksaur/AY1415/schema_update/target_programs/amico_updcode/amico_v12v20.so
