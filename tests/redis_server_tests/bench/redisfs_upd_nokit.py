24bc58561a119accd619026184e1c2f052d8ab12
ksaur <ksaur@umd.edu>
2015-07-06 15:58:39 -0400

    apparently redisfs v6 has a serious bug in the compression and was immediately replaced with v7, which is the same minus the bug

--- tests/redis_server_tests/bench/redisfs_upd_nokit.py
+++ tests/redis_server_tests/bench/redisfs_upd_nokit.py
@@ -10,11 +10,12 @@ from time import sleep
 
 kvolve_loc = "/fs/macdonald/ksaur/schema_update/redis-2.8.17/src/"
 impres_loc = "/fs/macdonald/ksaur/impressions-v1/impressions"
-impres_spec_loc = "/fs/macdonald/ksaur/impressions-v1/inputfile"
+impres_spec_loc = "/fs/macdonald/ksaur/schema_update/tests/redis_server_tests/bench/inputfile"
 redisfs_5_loc = "/fs/macdonald/ksaur/schema_update/target_programs/redisfs.5/src/redisfs"
 redisfs_6_loc = "/fs/macdonald/ksaur/schema_update/target_programs/redisfs.6/src/redisfs"
 upd_code = "/fs/macdonald/ksaur/schema_update/target_programs/redisfs_updcode/redisfs_v5v6.so"
 trials = 11
+migrating = 0
 
 def popen(args):
   print "$ %s" % args
@@ -25,7 +26,10 @@ def do_stats(r):
   f.write("Time\t#Queries\n")
   i = 0
   while True:
-    queries = r.info()["instantaneous_ops_per_sec"]
+    if migrating == 0:
+      queries = r.info()["instantaneous_ops_per_sec"]
+    else:
+      queries = 1
     f.write(str(i) + "\t" + str(queries-1) + "\n")
     time.sleep(.5)
     i = i + .5
@@ -35,6 +39,7 @@ def do_stats(r):
 def kv():
   print("______________KV_____________")
   for i in range (trials):
+    global migrating
     redis_server = popen(kvolve_loc +"redis-server " + kvolve_loc +"../redis.conf")
     sleep(1)
     r = redis.StrictRedis()
@@ -51,7 +56,17 @@ def kv():
     redisfs5.send_signal(signal.SIGINT)
     redisfs5.wait()
     r.client_setname("update/"+upd_code)
+    print "Migrating schema offline"
+    migrating = 1
+    allkeys = r.keys('*')
+    for k in allkeys:
+      typ = r.type(k)
+      if(typ == 'string'):
+        r.get(k)
+      elif(typ == 'set'):
+        r.smembers(k)
     print "RESUMING 6"
+    migrating = 0
     redisfs6 = popen(redisfs_6_loc)
     os.kill(bench.pid, signal.SIGCONT)
     bench.wait()
